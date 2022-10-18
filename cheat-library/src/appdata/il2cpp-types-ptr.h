3525d4180a4189b6fc130b9f745586575275db4f
xl000 <l_xb@foxmail.com>
2022-10-18 18:24:15 +0800

    3.1 fix

--- cheat-library/src/appdata/il2cpp-types-ptr.h
+++ cheat-library/src/appdata/il2cpp-types-ptr.h
@@ -5,24 +5,8 @@
 // * IL2CPP application-specific type definition addresses
 // ******************************************************************************
 
-DO_TYPEDEF(0x09E19C78, Int32);
-DO_TYPEDEF(0x09E19D88, Byte);
-DO_TYPEDEF(0x09E2CCC0, GameManager);
+DO_TYPEDEF(0x0A400E28, Int32);
 
-// v 3.0
-DO_TYPEDEF(0x09E389C8, LCChestPlugin);
-DO_TYPEDEF(0x09E29B98, SceneTreeObject);
-DO_TYPEDEF(0x09E40968, MoleMole_ActorModifier);
-
-//// v 2.6
-//DO_SINGLETONEDEF(0x0B3442D8, Singleton_1_MoleMole_MapModule_);
-//DO_SINGLETONEDEF(0x0B308870, Singleton_1_MoleMole_EntityManager_);
-//DO_SINGLETONEDEF(0x0B316A68, Singleton_1_MoleMole_LoadingManager_);
-//DO_SINGLETONEDEF(0x0B30BD10, Singleton_1_InteractionManager_);
-//DO_SINGLETONEDEF(0x0B2F8038, Singleton_1_MoleMole_UIManager_);
-//DO_SINGLETONEDEF(0x0B291838, Singleton_1_MoleMole_ItemModule_);
-//DO_SINGLETONEDEF(0x0B309120, Singleton_1_MoleMole_EventManager_);
-//DO_SINGLETONEDEF(0x0B326FF8, Singleton_1_MoleMole_PlayerModule_);
-//DO_SINGLETONEDEF(0x0B2F2A38, Singleton_1_MoleMole_MapManager_);
-//DO_SINGLETONEDEF(0x0B399890, Singleton_1_MoleMole_NetworkManager_);
-//DO_SINGLETONEDEF(0x0B2227B8, Singleton_1_MoleMole_ScenePropManager_);
+DO_TYPEDEF(0x0A447488, LCChestPlugin);
+DO_TYPEDEF(0x0A4106B8, SceneTreeObject);
+DO_TYPEDEF(0x0A4244F8, MoleMole_ActorModifier);
